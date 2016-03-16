898c5faf34a390d4ce5334d190d5f7823be98f5f
Jorden Hill <jhill4756@gmail.com>
2016-03-16 14:47:38 -0400

    Added test report
    Added test coverage report. VendingMachine and VendingMachineItem have
    100% coverage.

---
+++ VendingMachine/src/edu/towson/cis/cosc442/project3/vendingmachine/tests/VendingMachineItemTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package edu.towson.cis.cosc442.project3.vendingmachine.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.towson.cis.cosc442.project3.vendingmachine.*;
+
+/**
+ * @author jhill19
+ *
+ */
+public class VendingMachineItemTest {
+	
+	VendingMachineItem vendingMachineItem;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test to see if VendingMachineItem is constructed as intended.
+	 */
+	@Test
+	public final void testVendingMachineItem() {
+		vendingMachineItem = new VendingMachineItem("Coca-cola", 1.50);
+		assertNotNull("Unsuccessfully constructed vending machine item", vendingMachineItem);
+	}
+
+
+	/**
+	 * Test to see if an item with a negative price cannot be created.
+	 */
+	@Test (expected = VendingMachineException.class)
+	public final void testVendingMachineItemNegativePrice() {
+		vendingMachineItem = new VendingMachineItem("Coca-cola", -1.50);
+	}
+	
+	/**
+	 * Test  to see if proper name is returned from item.
+	 */
+	@Test
+	public final void testGetName() {
+		vendingMachineItem = new VendingMachineItem("Coca-cola", 1.50);
+		assertTrue("Returned incorrect item name.", "Coca-cola".equals(vendingMachineItem.getName()));
+	}
+
+	/**
+	 * Test to see if proper price is returned from item.
+	 */
+	@Test
+	public final void testGetPrice() {
+		vendingMachineItem = new VendingMachineItem("Coca-cola", 1.50);
+		assertEquals("Returned incorrect item price", 1.50, vendingMachineItem.getPrice(), 0.01);
+	}
+
+}
